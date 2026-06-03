c3558c54f06e75a1ae04290f1d4b3a0886237c97
pjpangilinan <patrickjpangilinan@protonmail.com>
2026-06-03 12:41:46 +0000

    Learned how to use external packages

---
+++ 1.1 Hello Go External Package/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+)
+
+func main() {
+	fmt.Println("Hello World from Go!")
+	color.Green("Hello World from Go! in green")
+	color.Blue("Hello World from Go! in blue")
+}
