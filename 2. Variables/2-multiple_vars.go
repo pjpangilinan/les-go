56657517d29acd207d097a546cffa28a611c00ce
pjpangilinan <patrickjpangilinan@protonmail.com>
2026-06-03 13:16:52 +0000

    Continuing variables

---
+++ 2. Variables/2-multiple_vars.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Explicitly declare variable type
+    var drinkName string = "Mountain Dew"
+    var size = "Large"
+   // Shorthand assignment and declaration
+   price := 50.00
+
+    fmt.Println(size, drinkName, "price is", price, "pesos.")
+}
